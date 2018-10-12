83f22ee5db909f97c38dd5792a9892ae2fbc9dde
YasinEhsan <yasinehsan11@gmail.com>
2018-10-12 17:39:49 -0400

    update photo map

---
+++ Photo_Map/Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
