0e26c1508ec6ea1c0e4710e51c8672e60de261be
YasinEhsan <yasinehsan11@gmail.com>
2018-12-03 08:48:55 -0500

    added biolerplate

--- flashChat/Flash Chat/AppDelegate.swift
+++ flashChat/Flash Chat/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        
+        FirebaseApp.configure()
         
         return true
     }
