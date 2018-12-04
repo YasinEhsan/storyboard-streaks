f313072d37a582d622890aed172a8fef84f32579
YasinEhsan <yasinehsan11@gmail.com>
2018-12-04 17:55:37 -0500

    basic setup done

---
+++ googleMapMarkers/CustomMarkerView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomMarkerView.swift
+//  
+//
+//  Created by A. Rodgers on 12/4/18.
+//
+
+import Foundation
---
+++ googleMapMarkers/googleMapMarkers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleMapMarkers
+//
+//  Created by A. Rodgers on 12/4/18.
+//  Copyright © 2018 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
