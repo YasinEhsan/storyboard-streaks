825e55766b0941ef9d7589cfd4c2928b5af2c573
YasinEhsan <yasinehsan11.com>
2019-01-28 20:17:26 -0500

    tip calc complete

---
+++ tipCalc2/tipCalc2Tests/tipCalc2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tipCalc2Tests.swift
+//  tipCalc2Tests
+//
+//  Created by A. Rodgers on 1/24/19.
+//  Copyright © 2019 Yasin Ehsan. All rights reserved.
+//
+
+import XCTest
+@testable import tipCalc2
+
+class tipCalc2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
