66b233208ebd4a069ffbfcf1ad9692a309e21eeb
YasinEhsan <yasinehsan11.com>
2019-02-27 01:46:16 -0500

    added pods + auto layout

--- Flix/Flix/MoviesViewController.swift
+++ Flix/Flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
    
@@ -61,6 +62,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell
     }
